8ceb166ffee824d37b39963e839f274d8616d95d
cyishere <cyishere@gmail.com>
2021-11-03 17:27:26 +0800

    Fix style bug

--- components/backstagetalks/GlobalStyles/GlobalStyles.ts
+++ components/backstagetalks/GlobalStyles/GlobalStyles.ts
@@ -4,27 +4,28 @@ const GlobalStyles = createGlobalStyle`
   /* ================== */
   /* Custom Properties  */
   /* ================== */
+  html {
+    /* colors */
+    --clr-cyan: #00c1b5;
+    --clr-yellow: #ffbe00;
+    --clr-red: #e30512;
+    --clr-orange: #ff651a;
+    --clr-blue: #1d3fbb;
+    --clr-black: #251d20;
+    --clr-white: #ffffff;
+    
+    /* font families */
+    --ff-primary: 'Roboto', sans-serif;
 
-  /* colors */
-  --clr-cyan: #00c1b5;
-  --clr-yellow: #ffbe00;
-  --clr-red: #e30512;
-  --clr-orange: #ff651a;
-  --clr-blue: #1d3fbb;
-  --clr-black: #251d20;
-  --clr-white: #ffffff;
-
-  /* font families */
-  --ff-primary: 'Roboto', sans-serif;
-
-  /* font sizes */
-  --fs-smaller: ${12 / 16}rem;
-  --fs-small: ${15 / 16}rem;
-  --fs-base: ${18 / 16}rem;
-
-  /* font weights */
-  --fw-normal: 400;
-  --fw-bold: 700;
+    /* font sizes */
+    --fs-smaller: ${12 / 16}rem;
+    --fs-small: ${15 / 16}rem;
+    --fs-base: ${18 / 16}rem;
+    
+    /* font weights */
+    --fw-normal: 400;
+    --fw-bold: 700;
+  }
 
   /* ================== */
   /* Global Styles      */
